cd3ef3144f63a27538161b63cd9a2b49516ed372
samiuddin00 <samiuddinkhan2000@gmail.com>
2023-02-16 14:24:29 +0530

    Added header folder and made  Users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import UserList from '../components/UserList';
+
 const Users = () => {
-    return <h2>Users is working</h2>;
+    const USERS =[
+        {id:'u1',name:'sami',image:'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg',places:4}
+    ]
+
+
+    return <UserList items={USERS}/>
 }
 
 export default Users;
